e766c1f9e8f9d178a86614b007778fa71eb96dd4
Fakhriddin Mullagoziev <mullagoziev@gmail.com>
2025-01-24 11:33:50 -0500

    Setting up JWT auth

--- learn-spring-security/src/main/java/com/in28minutes/learn_spring_security/jwt/JwtSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/in28minutes/learn_spring_security/jwt/JwtSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.in28minutes.learn_spring_security.basic;
+package com.in28minutes.learn_spring_security.jwt;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -9,77 +9,64 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-public class BasicAuthSecurityConfiguration {
+@Configuration
+public class JwtSecurityConfiguration {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 //		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
-		
+
 		http.csrf().disable();
 		http.headers().frameOptions().sameOrigin();
+		
+		http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+		
 		return http.build();
 	}
-	
-//	@Bean
-//	public UserDetailsService userDetailsService() {
-//		var user = User.withUsername("in28minutes")
-//		.password("{noop}dummy")
-//		.roles("USER")
-//		.build();
-//		
-//		var admin = User.withUsername("admin")
-//				.password("{noop}dummy")
-//				.roles("ADMIN")
-//				.build();
-//		
-//		
-//		return new InMemoryUserDetailsManager(user, admin);
-//	}
-	
+
 	@Bean
 	public DataSource dataSource() {
 		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
-				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-				.build();
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION).build();
 	}
-	
+
 	@Bean
 	public UserDetailsService userDetailsService(DataSource dataSource) {
 		var user = User.withUsername("in28minutes")
 //		.password("{noop}dummy")
-		.password("dummy")
-		.passwordEncoder(str -> passwordEncoder().encode(str))
-		.roles("USER")
-		.build();
-		
+				.password("dummy").passwordEncoder(str -> passwordEncoder().encode(str)).roles("USER").build();
+
 		var admin = User.withUsername("admin")
 //				.password("{noop}dummy")
-				.password("dummy")
-				.passwordEncoder(str -> passwordEncoder().encode(str))
-				.roles("ADMIN", "USER")
-				.build();
-		
+				.password("dummy").passwordEncoder(str -> passwordEncoder().encode(str)).roles("ADMIN", "USER").build();
+
 		var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 		jdbcUserDetailsManager.createUser(user);
 		jdbcUserDetailsManager.createUser(admin);
 		return jdbcUserDetailsManager;
 	}
-	
+
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder () {
-		return new BCryptPasswordEncoder(); 
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
+	
+//	@Bean
+//	public JwtDecoder jwtDecoder() {
+//		return decoder;
+//	}
 }
